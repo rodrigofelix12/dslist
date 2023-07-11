14813e3c60c5c5ea9bbda55977a03c30cbe618af
Rodrigo Felix <rodrigo.felix321@gmail.com>
2023-07-10 22:40:51 -0300

    Change return to DTO

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
@@ -14,9 +16,11 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	public List<Game> findAll() {
-		var result = gameRepository.findAll();
-		return result;
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream()
+				.map(x -> new GameMinDTO(x))
+				.collect(Collectors.toList());
 	}
 	
 }
